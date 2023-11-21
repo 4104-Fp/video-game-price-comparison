50cae86945dc17f4eb5439dcf8f862ce33df4aad
francoskiyo <133829249+francoskiyo@users.noreply.github.com>
2023-11-20 20:17:55 -0500

    I am adding an update
    Current project

--- main.py
+++ main.py
@@ -3,3 +3,26 @@ from datetime import datetime
 
 import requests
 import streamlit as st
+
+#Method of access to API
+url = 'https://www.cheapshark.com/api/1.0/games?'
+payload = {}
+headers = {}
+
+st.title('Deal Time')
+st.header('Deal Selection Time')
+
+game_name = st.text_input("Game Name")
+#url = 'https://www.cheapshark.com/api/1.0/games?title=batman'
+payload = {"title" : game_name}
+if game_name:
+    response = requests.request("GET", url, headers=headers, params=payload)
+    r_dict = response.json()
+    #response = requests.request("GET", 'https://www.cheapshark.com/api/1.0/games?title=batman', headers=headers, data=payload)
+    st.write(r_dict)
+    st.write(len(r_dict))
+    for x in range(len(r_dict)):
+        if r_dict[x]["thumb"] is "None":
+            st.warning("Does not exist")
+        else:
+            st.image(r_dict[x]["thumb"]) 
