e22a76944fe397a6c7a0393b1c18e49715c2c0cd
Martin Ivanoski <martinivanoski@Martins-MBP.lan>
2023-11-20 20:56:27 -0600

    Feature 1 and 2 are complited.

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-import json
-from datetime import datetime
-
 import requests
-import streamlit as st
+
+from src.display_charts import *
 
 # Method of access to API
 url = 'https://www.cheapshark.com/api/1.0/games?'
-payload = {}
 headers = {}
 
 st.title('Deal Time')
@@ -25,6 +22,10 @@ if game_name:
         img = item.get('thumb')
         try:
             if img:  # Check if img is not None
+                # get name of game
+                select_game = st.checkbox(item.get('external', 'Unknown'), key=item.get('external', 'Unknown'))
+                if select_game:
+                    display_writing()
                 st.image(img, width=200)
         except Exception as e:
             st.warning(f"Could not display image for {item.get('external', 'Unknown')} game. Error: {e}")
---
+++ src/display_charts.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+
+def display_writing():
+    st.write("## Writing")
