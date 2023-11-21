b2c3ddbdd3816ba32f819418100b8455ebbb3600
Martin Ivanoski <martinivanoski@Martins-MBP.lan>
2023-11-20 21:37:06 -0600

    Formatted the code a lil bit, created functions for displaying charts, seperated each chart in seperate functions

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from src.display_charts import *
 
 # Method of access to API
 url = 'https://www.cheapshark.com/api/1.0/games?'
-headers = {}
 
 st.title('Deal Time')
 st.header('Deal Selection Time')
@@ -15,7 +14,7 @@ payload = {"title": game_name}
 if game_name:
     # response = requests.request("GET", 'https://www.cheapshark.com/api/1.0/games?title=batman', headers=headers,
     # data=payload)
-    response = requests.request("GET", url, headers=headers, params=payload)
+    response = requests.request("GET", url, params=payload)
     r_dict = response.json()
     st.write(len(r_dict))
     for item in r_dict:
@@ -23,9 +22,14 @@ if game_name:
         try:
             if img:  # Check if img is not None
                 # get name of game
-                select_game = st.checkbox(item.get('external', 'Unknown'), key=item.get('external', 'Unknown'))
+                game_name = item.get('external', 'Unknown')
+                select_game = st.checkbox(game_name, key=game_name)
                 if select_game:
-                    display_writing()
+                    display_bar_chart()
+                    display_line_chart()
+                    display_table()
                 st.image(img, width=200)
         except Exception as e:
-            st.warning(f"Could not display image for {item.get('external', 'Unknown')} game. Error: {e}")
+            # display error in console
+            print(f"No Thumbnail Found, {e}, for {item.get('external', 'Unknown')}")
+            st.warning("No Thumbnail Found")
--- src/display_charts.py
+++ src/display_charts.py
@@ -1,5 +1,22 @@
 import streamlit as st
 
 
-def display_writing():
-    st.write("## Writing")
+# Feature-API-Call #3 (Name):
+#   - Bar chart comparing store discounted price
+
+def display_bar_chart():
+    st.warning("Bar Chart")
+
+
+# Feature-API-Call #4 (Martin):
+#   - Line Chart compare the cheapest store prices vs cheapest historical price
+
+def display_line_chart():
+    st.info("Line Chart")
+
+
+# Feature-API-Call #5 (Name):
+#   - Include all stores for an interactive table
+
+def display_table():
+    st.success("Table")
