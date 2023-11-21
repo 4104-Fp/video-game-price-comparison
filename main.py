bac073e2f77270bd69749511f65380d1269dbb41
Martin Ivanoski <martinivanoski@Martins-MBP.lan>
2023-11-20 19:45:13 -0600

    Fixing the issue with not displaying the image.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import requests
 import streamlit as st
 
-#Method of access to API
+# Method of access to API
 url = 'https://www.cheapshark.com/api/1.0/games?'
 payload = {}
 headers = {}
@@ -13,16 +13,19 @@ st.title('Deal Time')
 st.header('Deal Selection Time')
 
 game_name = st.text_input("Game Name")
-#url = 'https://www.cheapshark.com/api/1.0/games?title=batman'
-payload = {"title" : game_name}
+# url = 'https://www.cheapshark.com/api/1.0/games?title=batman'
+payload = {"title": game_name}
 if game_name:
+    # response = requests.request("GET", 'https://www.cheapshark.com/api/1.0/games?title=batman', headers=headers,
+    # data=payload)
     response = requests.request("GET", url, headers=headers, params=payload)
     r_dict = response.json()
-    #response = requests.request("GET", 'https://www.cheapshark.com/api/1.0/games?title=batman', headers=headers, data=payload)
-    st.write(r_dict)
+    # st.write(r_dict)
     st.write(len(r_dict))
-    for x in range(len(r_dict)):
-        if r_dict[x]["thumb"] is "None":
-            st.warning("Does not exist")
-        else:
-            st.image(r_dict[x]["thumb"])
+    for item in r_dict:
+        img = item.get('thumb')
+        try:
+            if img:  # Check if img is not None
+                st.image(img)
+        except Exception as e:
+            st.warning(f"Could not display image for {item.get('external', 'Unknown')} game. Error: {e}")
