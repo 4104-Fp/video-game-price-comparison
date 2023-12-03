27200a90923eec2bd7fde8e68a74a6dcb2a8ad12
nlima21 <110039165+nlima21@users.noreply.github.com>
2023-12-03 15:33:49 -0500

    Update display_charts.py

--- src/utils/display_charts.py
+++ src/utils/display_charts.py
@@ -7,9 +7,6 @@ import streamlit as st
 # Feature-API-Call #3 (Name):
 #   - Bar chart comparing store discounted price
 def display_bar_chart(game_id):
-res_dict = requests.request(
-        "GET", f"https://www.cheapshark.com/api/1.0/games?id={game_id}"
-    ).json()
 st.warning("Barchart")
 
 def display_line_chart(game_id):
